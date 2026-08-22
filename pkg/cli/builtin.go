f473ede02703c287482c214d8dfcaf43b5adb403
berquerant <sin@sin-pro.local>
2026-08-22 18:14:14 +0900

    Sort through unwanted presets

--- pkg/cli/builtin.go
+++ pkg/cli/builtin.go
@@ -7,56 +7,12 @@ func builtinConfigSet() *ConfigSet {
 				Preprocess: []string{`jq -S .`},
 				Diff:       "diff -u --color",
 			},
-			"yml": &Config{
+			"yaml": &Config{
 				Preprocess: []string{`yq -P 'sort_keys(..)'`},
 				Diff:       "diff -u --color",
 			},
-			"k8s": &Config{
+			"objdiff": &Config{
 				Diff: "objdiff -cv",
-			},
-			"k8s-clean": &Config{
-				Preprocess: []string{
-					`yq 'del(.metadata.resourceVersion, .metadata.uid, .metadata.creationTimestamp, .metadata.generation, .metadata.managedFields, .status)'`,
-				},
-				Diff: "objdiff -cv",
-			},
-			"helm": &Config{
-				Diff: "objdiff -cv",
-				Startup: []string{
-					`helm repo update`,
-				},
-			},
-			"helm-chart": &Config{
-				Diff: "diff -u --color",
-				Startup: []string{
-					`helm repo update`,
-				},
-				Preprocess: []string{`yq -P 'sort_keys(..)'`},
-				CommonArgs: []string{
-					"helm", "show", "chart", "$CHART",
-				},
-				LeftArgs: []string{
-					"--version", "$LEFT",
-				},
-				RightArgs: []string{
-					"--version", "$RIGHT",
-				},
-			},
-			"helm-values": &Config{
-				Diff: "diff -u --color",
-				Startup: []string{
-					`helm repo update`,
-				},
-				Preprocess: []string{`yq -P 'sort_keys(..)'`},
-				CommonArgs: []string{
-					"helm", "show", "values", "$CHART",
-				},
-				LeftArgs: []string{
-					"--version", "$LEFT",
-				},
-				RightArgs: []string{
-					"--version", "$RIGHT",
-				},
 			},
 			"dyff": &Config{
 				Diff: "dyff between --omit-header --set-exit-code",
